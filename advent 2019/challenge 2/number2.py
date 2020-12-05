6b09da3a60d56b2bfd779b58af79c2dac646671c
Kai van Dalen <kaid@topdesk.com>
2020-12-06 00:54:06 +0100

    Number 2 solved!

--- advent 2019/challenge 2/number2.py
+++ advent 2019/challenge 2/number2.py
@@ -5,11 +5,26 @@ def iterator(input_list, starting_pos=0):
     if positions[0] == 2:
         input_list[positions[3]] = input_list[positions[1]] * input_list[positions[2]]
 
-    if positions[0] == 99 or starting_pos + 4 >= len(input_list):
-        print("Got output: {}".format(input_list))
+    if positions[0] == 99 or starting_pos + 3 >= len(input_list):
         return input_list
 
     return iterator(input_list, starting_pos+4)
+
+
+def input_wrapper(code, puzzle_part):
+    if puzzle_part == 1:
+        code[1] = 12
+        code[2] = 2
+        return iterator(code)
+
+    for noun in range(100):
+        for verb in range(100):
+            code = get_input()
+            code[1] = noun
+            code[2] = verb
+            outcome = iterator(code)[0]
+            if outcome == 19690720:
+                return noun * 100 + verb
 
 
 def get_input():
@@ -19,10 +34,10 @@ def get_input():
 
 def main():
     code = get_input()
-    code[1] = 12
-    code[2] = 2
+
     print("Input: {}".format(code))
-    print("Final outcome: {}".format(iterator(code)[0]))
+    print("Final outcome: {}".format(input_wrapper(code, 1)[0]))
+    print("Final final outcome: {}".format(input_wrapper(code, 2)))
 
 
 if __name__ == "__main__":
