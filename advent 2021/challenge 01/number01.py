614df506c3ba1d636137c3b684cf7e0401cd5d76
Jimdewit <kaivandalen@gmail.com>
2021-12-01 07:02:54 +0100

    Solve day 1; accept off-by-one error

---
+++ advent 2021/challenge 01/number01.py
@@ -0,0 +1,40 @@
+
+
+def get_input():
+    with open('./input.txt', 'r') as input_file:
+        lines = [l.strip('\n') for l in input_file.readlines()]
+    return lines
+
+
+def process_depth(depth_list, use_sliding_window=False):
+    increase_counter = 0
+    if not use_sliding_window:
+        previous_depth = depth_list[0]
+        for depth in depth_list:
+            # Using > weirdly leads to an off-by-one; >= returns the correct number?!
+            if depth >= previous_depth:
+                increase_counter += 1
+            previous_depth = depth
+        return increase_counter
+    else:
+        starting_pos = 0
+        previous_window = sum(int(x) for x in depth_list[starting_pos:starting_pos+3])
+        for depth in depth_list:
+            current_window = sum(int(x) for x in depth_list[starting_pos:starting_pos+3])
+            if current_window > previous_window:
+                increase_counter +=1
+            previous_window = current_window
+            starting_pos += 1
+        return increase_counter
+
+
+def solve():
+    depth_list = get_input()
+    increment_counter = process_depth(depth_list)
+    print(increment_counter)
+    increment_counter = process_depth(depth_list, use_sliding_window=True)
+    print(increment_counter)
+
+
+if __name__ == "__main__":
+    solve()
---
+++ advent 2021/challenge 01/test01.py
@@ -0,0 +1,11 @@
+from number01 import process_depth
+
+
+def test_first_result():
+    increase = process_depth([int(f.strip('\n')) for f in open('./test_input.txt').readlines()])
+    assert increase == 7
+
+
+def test_second_result():
+    increase = process_depth([int(f.strip('\n')) for f in open('./test_input.txt').readlines()], use_sliding_window=True)
+    assert increase == 5
