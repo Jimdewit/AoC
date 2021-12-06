c3fe0cf458bac998c870deb52674936f53d48c14
Jimdewit <kaivandalen@gmail.com>
2021-12-06 14:01:38 +0100

    2021 - Day 6: Further prettify solution

--- advent 2021/challenge 06/number06.py
+++ advent 2021/challenge 06/number06.py
@@ -1,26 +1,16 @@
-import datetime
+# 345793
+# 1572643095893
 
 
-def process_pregnancy(fishes, more_fish={}, day_counter=1, max_days=80):
-
-    if day_counter == 1:
-        more_fish = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0}
-        for fish in fishes:
-            if fish == 0:
-                more_fish[8] += 1
-                more_fish[6] += 1
-            else:
-                more_fish[fish-1] += 1
-    else:
-        fish_giving_birth = more_fish[0]
-
-        for x in range(0,8):
-            more_fish[x] = more_fish[x+1] if x != 6 else (more_fish[x+1] + fish_giving_birth)
-        more_fish[8] = fish_giving_birth
+def process_pregnancy(more_fish, day_counter=1, max_days=80):
+    fish_giving_birth = more_fish[0]
+    for x in range(0,8):
+        more_fish[x] = more_fish[x+1] if x != 6 else (more_fish[x+1] + fish_giving_birth)
+    more_fish[8] = fish_giving_birth
 
     if day_counter < max_days:
         day_counter += 1
-        return process_pregnancy(fishes, more_fish, day_counter, max_days=max_days)
+        return process_pregnancy(more_fish, day_counter, max_days=max_days)
     else:
         return sum(v for v in more_fish.values())
 
@@ -28,14 +18,17 @@ def process_pregnancy(fishes, more_fish={}, day_counter=1, max_days=80):
 def get_input():
     with open('./input.txt', 'r') as input_file:
         lines = [int(x) for x in [l.strip('\n').split(',') for l in input_file.readlines()][0]]
-    return lines
+        more_fish = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0}
+        for fish in lines:
+            more_fish[fish] += 1
+    return more_fish
 
 
 def solve():
     fishes = get_input()
     more_fishes = process_pregnancy(fishes)
     print(more_fishes)
-    even_more_fishes = process_pregnancy(fishes, max_days=256)
+    even_more_fishes = process_pregnancy(fishes, max_days=255)
     print(even_more_fishes)
 
 
