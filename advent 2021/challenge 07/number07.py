c59c8b8b362b7f95e90a6f26e308329624909a16
Jimdewit <kaivandalen@gmail.com>
2021-12-07 08:49:56 +0100

    2021 - Day 7: Solve

---
+++ advent 2021/challenge 07/number07.py
@@ -0,0 +1,44 @@
+import statistics
+
+
+def find_middle_number(input_list):
+    middle = float(len(input_list))/2.
+    if middle % 2 != 0:
+        return input_list[int(middle - .5)]
+    else:
+        return input_list[int(middle)], input_list[int(middle-1)]
+
+
+def check_positions(list_of_positions, increase_expense=False):
+    fuel_sum = 0
+    if not increase_expense:
+        median = int(statistics.median(list_of_positions))
+        print(int(median))
+        for x in list_of_positions:
+            fuel_sum += median - x if median > x else x - median
+    else:
+        # Rounding off high or low can probably be found in a neater way
+        mean = int(round(statistics.mean(list_of_positions)))-1
+        for x in list_of_positions:
+            dx = mean - x if mean > x else x - mean
+            fuel_sum += sum(range(dx+1))
+
+    return fuel_sum
+
+
+def get_input():
+    with open('./input.txt', 'r') as input_file:
+        lines = [int(l) for l in input_file.read().strip('\n').split(',')]
+    return lines
+
+
+def solve():
+    daily_input = get_input()
+    total_fuel = check_positions(daily_input)
+    print('Crab fuel used: {}'.format(total_fuel))
+    more_expensive_fuel = check_positions(daily_input, increase_expense=True)
+    print('More expensive fuel used: {}'.format(more_expensive_fuel))
+
+
+if __name__ == "__main__":
+    solve()
