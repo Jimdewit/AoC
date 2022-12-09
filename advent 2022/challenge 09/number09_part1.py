7930625d0a6a950ecdbcc65cf586dd6ae94958cf
Kai van Dalen <kaid@topdesk.com>
2022-12-09 10:51:18 +0100

    2022 Day 9: Rename files, remove debug lines

--- advent 2022/challenge 09/number09_part1.py
+++ advent 2022/challenge 09/number09_part1.py
@@ -4,16 +4,13 @@ def translate_tail(head_positions, tail_positions):
     head_x, head_y = head_pos[0], head_pos[1]
     tail_x, tail_y = tail_pos[0], tail_pos[1]
     if abs(head_x - tail_x) <= 1 and abs(head_y - tail_y) <= 1:
-        # print('No tail adjustment needed')
         return tail_positions
     else:
-        # print('Moving tail to {}'.format(head_positions[-2]))
         tail_positions.append(head_positions[-2])
         return tail_positions
 
 
 def translate_x(amount, head_positions, tail_positions, step):
-    print('translating {} over x, heads at {}'.format(amount, head_positions))
     start_x = head_positions[-1][0]
     start_y = head_positions[-1][1]
     for x in range(start_x+step, start_x+amount, step):
@@ -51,22 +48,19 @@ def process_instructions(instruction_set):
     tail_positions = [(0, 0)]
     for translation in instruction_set:
         head_positions, tail_positions = translate_head(translation, head_positions, tail_positions)
-        print('Finished translation, head at {} tail at {}'.format(head_positions, tail_positions))
     print(len(set(tail_positions)))
 
 def get_input():
-    with open('./input.txt', 'r') as input_file:
+    with open('./test_input_part1.txt', 'r') as input_file:
         instructions = []
         lines = [l.strip('\n') for l in input_file.readlines()]
         for l in lines:
             instructions.append(tuple(i for i in l.split()))
-    print(instructions)
     return instructions
 
 
 def solve():
     instruction_set = get_input()
-
     process_instructions(instruction_set)
 
 
