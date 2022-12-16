67a01b4a1f942d64143c24155d597864f0f6b867
Kai van Dalen <kaid@topdesk.com>
2022-12-16 10:49:07 +0100

    2022 Day 13 (part 1): More almost solved

--- advent 2022/challenge 13/number13.py
+++ advent 2022/challenge 13/number13.py
@@ -3,33 +3,32 @@ import json
 
 def process_list_and_list(p1, p2):
     # print(f'Processing {p1} and {p2}')
-    for x in range(0, len(p1)):
-        try:
-            if type(p1[x]) != type(p2[x]):
-                #print(f'{type(p1[x])} for {p1[x]} and {type(p2[x])} for {p2[x]} are not compatible; harmonizing!')
-                return harmonize_and_process_pair(p1[x], p2[x])
-            if isinstance(p1[x], list) and isinstance(p2[x], list):
-                #print(f'Unpacking lists {p1[x]} and {p2[x]}')
-                return harmonize_and_process_pair(p1[x], p2[x])
-            if p1[x] > p2[x]:
+    left_is_bigger = True
+    while left_is_bigger:
+        for x in range(0, len(p1)):
+            try:
+                if type(p1[x]) != type(p2[x]):
+                    #print(f'{type(p1[x])} for {p1[x]} and {type(p2[x])} for {p2[x]} are not compatible; harmonizing!')
+                    return harmonize_and_process_pair(p1[x], p2[x])
+                if isinstance(p1[x], list) and isinstance(p2[x], list):
+                    #print(f'Unpacking lists {p1[x]} and {p2[x]}')
+                    return process_list_and_list(p1[x], p2[x])
+                if p1[x] > p2[x]:
+                    left_is_bigger = False
+                if p1[x] < p2[x]:
+                    print(f'Smaller is true for {p1[x]} AND {p2[x]}\n(from lists {p1} AND {p2})\n')
+                    left_is_bigger = True
+            except IndexError as e:
+                # Return False if p2[x] cannot be bound
                 return False
-            if p1[x] < p2[x]:
-                #print(f'Smaller is true for {p1[x]} AND {p2[x]}\n(from lists {p1} AND {p2})\n')
-                return True
-        except IndexError as e:
-            return False
-        # if x == 0 and len(p1) == len(p2) and p1[x] == p2[x]:
-        #     return False
 
-    print(f'Left {p1} ran out faster than {p2}')
+        print(f'Left {p1} ran out faster than {p2}')
 
-    return True
+        return left_is_bigger
 
 def process_int_and_int(p1, p2):
-    if p1 < p2:
-        return True
-    else:
-        return False
+    print(f'Smaller is true for {p1} AND {p2}\n')
+    return p1 < p2
 
 
 def process_int_and_list(p1, p2):
@@ -53,12 +52,12 @@ def harmonize_and_process_pair(p1, p2):
 
 
 def process_pairs(list_of_pairs):
-    list_counter = 0
+    list_counter = 1
     valid_counter = 0
     for p1, p2 in list_of_pairs:
         is_valid_message = harmonize_and_process_pair(p1, p2)
         if is_valid_message:
-            # print(f'counter {valid_counter} + {list_counter} because p1 smaller is {is_valid_message} for {p1} and {p2}')
+            print(f'counter {valid_counter} + {list_counter} because p1 smaller is {is_valid_message} for {p1} and {p2}')
             valid_counter += list_counter
         list_counter += 1
     print(valid_counter)
